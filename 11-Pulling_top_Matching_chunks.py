7e7c4347b3a52239d39d539beaf4635f04f8fc91
Sunny Kumar <129001632+sunnykr-7@users.noreply.github.com>
2026-05-13 11:51:29 +0530

    Add files via upload

---
+++ 11-Pulling_top_Matching_chunks.py
@@ -0,0 +1,60 @@
+import requests
+import os
+import json
+import pandas as pd
+import numpy as np
+from sklearn.metrics.pairwise import cosine_similarity
+
+def create_embedding(text_list):
+    r = requests.post("http://localhost:11434/api/embed", json={
+        "model": "bge-m3", 
+        "input": text_list
+    })
+
+    embedding = r.json()["embeddings"]
+    return embedding
+
+jsons = os.listdir("jsons") # List all the Jsons
+my_dicts = []
+chunk_id = 0
+
+
+for json_file in jsons:
+    with open(f"jsons/{json_file}") as f:
+        content = json.load(f)
+    print(f"Creating Embeddings for {json_file}")
+    embeddings = create_embedding([c['text'] for c in content['chunks']])
+
+    for i, chunk in enumerate(content['chunks']):
+        chunk['chunk_id'] = chunk_id
+        chunk['embedding'] = embeddings[i]
+        chunk_id +=1
+        my_dicts.append(chunk)
+    break
+ 
+
+
+df = pd.DataFrame.from_records(my_dicts)
+
+
+incoming_query = input("Ask a Question: ")
+question_embedding = create_embedding([incoming_query])[0]
+
+
+
+# Find similarities of question_embeddin with other embeddings
+similarities = cosine_similarity(np.vstack(df['embedding']), [question_embedding]).flatten()
+
+print(similarities)
+
+top_results = 3
+max_indx = similarities.argsort()[::-1][0:top_results]
+print(max_indx)
+new_df = df.loc[max_indx]
+print(new_df[["title", "number", "text"]])
+
+
+
+
+
+
