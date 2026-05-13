c653908da31caec481782b5ad96385c6df4bc0f8
Sunny Kumar <129001632+sunnykr-7@users.noreply.github.com>
2026-05-13 11:52:24 +0530

    added 14 file

---
+++ 14-Creating_prompt_for_our_RAG_based_System.py
@@ -0,0 +1,53 @@
+import pandas as pd
+import numpy as np
+from sklearn.metrics.pairwise import cosine_similarity
+import joblib
+import requests
+
+
+def create_embedding(text_list):
+    r = requests.post("http://localhost:11434/api/embed", json={
+        "model": "bge-m3", 
+        "input": text_list
+    })
+
+    embedding = r.json()["embeddings"]
+    return embedding
+
+
+
+df = joblib.load('embeddings.joblib')
+
+
+incoming_query = input("Ask a Question: ")
+question_embedding = create_embedding([incoming_query])[0]
+
+
+
+# Find similarities of question_embeddin with other embeddings
+similarities = cosine_similarity(np.vstack(df['embedding']), [question_embedding]).flatten()
+
+
+
+top_results = 5
+max_indx = similarities.argsort()[::-1][0:top_results]
+
+new_df = df.loc[max_indx]
+
+
+
+prompt = f''' I am teaching AI using SK AI course. Here are video subtitle chunks containing video title, video number, start time in second, end time in seconds, the text at that time:
+
+{new_df[["title", "number", "start", "end", "text"]].to_json()}
+--------------------------------------
+
+"{incoming_query}"
+User asked this question related to the video chunks, you have to answer where and how much content is taught in which video (in which video and at what timestamp) and guide the user to go to that particular video. If user asks unrelated question, tell him that you can only answer questions related to that course
+
+'''
+
+
+
+with open("14-prompt.txt", "w") as f:
+    f.write(prompt)
+
