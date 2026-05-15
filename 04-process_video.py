aa52ba0ec7e7ff2bf6dbaa16656bb3081802c627
Sunny Kumar <129001632+sunnykr-7@users.noreply.github.com>
2026-05-15 21:45:38 +0530

    Update 04-process_video.py

--- 04-process_video.py
+++ 04-process_video.py
@@ -2,11 +2,11 @@ import os
 import subprocess
 files = os.listdir("Videos")
 
-
 for file in files:
     tutorial_number = file.split(" [")[0].split(" #")[1]
     file_name = file.split(" - ")[0]
     print(tutorial_number, file_name)
+
 
     subprocess.run(["ffmpeg", "-i", f"videos/{file}", f"audios/{tutorial_number}_{file_name}.mp3"])
 
